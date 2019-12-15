13a1c7d850ee7a7466c8a1bcbeed3b75f83711c2
kaimi <kainni07@gmail.com>
2019-12-15 21:11:11 +0900

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('Ethan', 'sample@sample.com', 'Hello', '2019-11-22 15:18:20');
+
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('Emma', 'sample2@sample.com', 'Good Morning', '2019-11-23 16:30:29');
+
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('William', 'sample3@sample.com', 'Good Evening', '2019-11-20 11:11:11');
+
